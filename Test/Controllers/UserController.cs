5f2fa5fcc1d9401bef89eb36cbb4d69fe03fef72
Adil AlShihi <safeme2010@hotmail.com>
2021-02-01 13:42:00 +0400

    initialize the project

---
+++ Test/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Test.Models;
+
+namespace Test.Controllers
+{
+   // http://localhost:4256/api/user
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetUser()
+        {
+            var user = new User { Id = 1111, Name = "Adel" };
+            return Ok(user);
+        }
+
+
+        [HttpPost]
+        public ActionResult AddUser(User user)
+        {
+            var newUser = new User { Id = 1111, Name = "Adel2" };
+            return Ok(newUser);
+        }
+    }
+}
